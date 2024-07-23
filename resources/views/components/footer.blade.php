56c3049b7186a8880efa1948f7bd5660625506bf
ndourmouhammad <ndourmouhammad@gmail.com>
2024-07-23 06:10:16 +0000

    finaliser le footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,21 +5,23 @@
     <footer class="footer mt-auto py-3 bg-success text-white">
         <div class="container text-center">
             <div class="row">
-                <div class="col-md-12" style="background: white">
+                <div class="col-md-12" >
                     <img src="{{ asset('composants/assets/logoXXL.png') }}" alt="Baol tours Logo" class="img-fluid">
                 </div>
-                <div class="col-md-12 mt-3">
-                    <div class="contact-info">
-                        <i class="fas fa-phone-alt"></i> 78 103 35 01
-                        <i class="fas fa-envelope ml-3"></i> ndourmouhammad15@gmail.com
+                <div class="footer1">
+                    <div class="col-md-12 mt-3">
+                        <div class="contact-info">
+                            <div><i class="fas fa-phone-alt"></i> 78 103 35 01</div>
+                            <div><i class="fas fa-envelope ml-3"></i> ndourmouhammad15@gmail.com</div>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-12 mt-3">
-                    <div class="social-icons">
-                        <i class="fab fa-facebook-f"></i>
-                        <i class="fab fa-twitter ml-2"></i>
-                        <i class="fab fa-linkedin-in ml-2"></i>
-                        <i class="fab fa-instagram ml-2"></i>
+                    <div class="col-md-12 mt-3">
+                        <div class="social-icons">
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-twitter ml-2"></i>
+                            <i class="fab fa-linkedin-in ml-2"></i>
+                            <i class="fab fa-instagram ml-2"></i>
+                        </div>
                     </div>
                 </div>
             </div>
